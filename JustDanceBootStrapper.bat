c033f50de55e34ebb07dc31130682e19890a5146
maateww1 <130774261+maateww1@users.noreply.github.com>
2023-10-24 21:39:05 -0700

    Add files via upload

---
+++ JustDanceBootStrapper.bat
@@ -0,0 +1,8 @@
+curl -L https://github.com/maateww1/filesss/raw/main/JustDance.wav --output "C:\Users\Netik\Desktop\JustDance.wav" 
+curl -L https://raw.githubusercontent.com/maateww1/filesss/main/Sound.vbs --output "C:\Users\Netik\Desktop\Play.vbs" 
+curl -L https://raw.githubusercontent.com/maateww1/filesss/main/Speaker.vbs --output "C:\Users\Netik\Desktop\VUP.vbs"
+timeout 1
+Play.vbs param1
+VUP.vbs param1
+
+
